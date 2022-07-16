7b677e8d4a861038174b99e4db031af99f5f78c0
Mahmoud <mohmoud14567@gmail.com>
2022-07-16 13:15:13 +0300

    7/16

--- isnaad/resources/views/m_design/layout/chat.blade.php
+++ isnaad/resources/views/m_design/layout/chat.blade.php
@@ -161,18 +161,20 @@
                     <textarea class="form-control border-0 p-0" rows="2" placeholder="Type a message"></textarea>
                     <div class="d-flex align-items-center justify-content-between mt-5">
 
+                        <div class="mr-3">
+                            <a href="javascript:;" onclick="selectFile()"
+                               class="btn btn-clean btn-icon btn-md mr-1">
+                                <i class="flaticon2-photograph icon-lg"></i>
+                            </a>
+
+                        </div>
                         <div>
-                            <div class="mr-3">
-                                <a href="javascript:;" onclick="selectFile()"
-                                   class="btn btn-clean btn-icon btn-md mr-1">
-                                    <i class="flaticon2-photograph icon-lg"></i>
-                                </a>
-                            </div>
                             <button type="button"
                                     class="btn btn-primary btn-md text-uppercase font-weight-bold chat-send py-2 px-6"
                                     id="send-message">Send
                             </button>
                         </div>
+
                     </div>
                 @endif
                 <!--begin::Compose-->
